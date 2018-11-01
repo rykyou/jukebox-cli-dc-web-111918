84652903dde636a033ca64cbc73ec418bb1103c5
Robin Kim <rkim1214@gmail.com>
2018-11-01 16:01:22 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -7,9 +7,9 @@
 # "LiberTeens" => '/home/organized-revert-5689/jukebox-cli-dc-web-111918/lib/jukebox-cli/audio/Emerald-Park/02.mp3',
 # "Hamburg" =>  '/home/organized-revert-5689/jukebox-cli-dc-web-111918/lib/jukebox-cli/audio/Emerald-Park/03.mp3',
 # "Guiding Light" => '/home/organized-revert-5689/jukebox-cli-dc-web-111918/lib/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '/home/organized-revert-5689/jukebox-cli-dc-web-111918jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Wolf" => '/home/organized-revert-5689/jukebox-cli-dc-web-111918/lib/jukebox-cli/audio/Emerald-Park/05.mp3',
+# "Blue" => '/home/organized-revert-5689/jukebox-cli-dc-web-111918/lib/jukebox-cli/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => '/home/organized-revert-5689/jukebox-cli-dc-web-111918/lib/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
 def help
